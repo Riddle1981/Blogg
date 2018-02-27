8a298b3187b80c6373df98dbf87fe7217af1f55d
jielang <jielang19940627@163.com>
2018-02-27 23:22:10 +0800

    c

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,9 +21,17 @@ module.exports = {
   ],
   // add your custom rules here
   rules: {
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
     // allow async-await
-    'generator-star-spacing': 'off',
+    'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-tabs': 0,
+    'no-mixed-spaces-and-tabs': 0,
+    'indent': ["off", "tab"],
+    'no-unused-vars': 0,
+    'no-trailing-spaces': 0,
+    "eqeqeq": 0
   }
 }
